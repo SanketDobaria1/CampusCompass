79e6f26021be3311d92cd0c0de1448c29c90661e
Priyank Thakur <priyankthakur3@live.com>
2023-03-30 13:56:52 -0400

    Initialized express and layouts

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-//import bandRoutes from './bands.js';
-//import albumRoutes from './albums.js';
-
 const constructorMethod = (app) => {
-    app.use('/', (req, res) => {
-        res.json('Hello World')
-    });
-    app.use('*', (req, res) => {
-        res.status(404).json('404 : Not found');
-    });
+  app.use("/", (req, res) => {
+    res.render("layouts/main");
+  });
+
+  app.use("/landing", (req, res) => {
+    res.sendFile("maps/landing.html");
+  });
+  app.use("*", (req, res) => {
+    res.status(404).json("404 : Not found");
+  });
 };
 
 export default constructorMethod;
