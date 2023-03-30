6492c02c2385c69bc7958f8ff430a282c1b8c9fd
Priyank Thakur <priyankthakur3@live.com>
2023-03-29 23:17:17 -0400

    Added New collections

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -15,3 +15,7 @@ const getCollectionFn = (collection) => {
 
 /* Now, you can list your collections here: */
 export const locations = getCollectionFn("locations");
+export const users = getCollectionFn("users");
+export const feedback = getCollectionFn("feedback");
+export const events = getCollectionFn("events");
+export const department = getCollectionFn("department");
