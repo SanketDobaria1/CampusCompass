179ed1b437a6d4ca1a407af20a15ebfb496ab755
Priyank Thakur <priyankthakur3@live.com>
2023-03-29 23:18:16 -0400

    testing file

--- tests/test.js
+++
@@ -1,6 +0,0 @@
-import { locationsDataFunction } from "../data/index.js";
-import { closeConnection } from "../config/mongoConnection.js";
-
-console.log(await locationsDataFunction.getAll());
-
-await closeConnection();
