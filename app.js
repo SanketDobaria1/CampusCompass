c6b3f130817799ab670c539cf076f7fed5c27a1d
Priyank Thakur <priyankthakur3@live.com>
2023-04-07 18:56:48 -0400

    added session

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import session from "express-session";
 const app = express();
 import configRoutes from "./routes/index.js";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+
 import exphbs from "express-handlebars";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,12 +16,20 @@ const port = 3000;
 app.use("/public", staticDir);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    name: "AuthCookie",
+    secret: "CampusCompass",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.engine(
   "handlebars",
   exphbs.engine({
     defaultLayout: "main",
-    partialsDir: [path.join(__dirname + "views/partials")],
+    partialsDir: [path.join(__dirname + "/views/partials")],
   })
 );
 app.set("view engine", "handlebars");
