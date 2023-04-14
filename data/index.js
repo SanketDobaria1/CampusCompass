a72a332a799e44ab0bd8671a162e97f96d560a86
Priyank Thakur <priyankthakur3@live.com>
2023-04-14 19:12:30 -0400

    fixed export

--- data/index.js
+++ data/index.js
@@ -2,8 +2,6 @@ import locationsDataFunction from "./locations.js";
 import roomsDataFunction from "./rooms.js";
 import userDataFunction from "./users.js";
 
-const userData = userDataFunction;
-const roomsData = roomsDataFunction;
-const locationsData = locationsDataFunction;
-
-export default { locationsData, roomsData, userData };
+export const locationsData = locationsDataFunction;
+export const roomsData = roomsDataFunction;
+export const userData = userDataFunction;
