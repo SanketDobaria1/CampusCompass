22c21f9c5e4cddd9277fb9272c3d3043bc9ec9a5
Priyank Thakur <priyankthakur3@live.com>
2023-04-14 19:15:21 -0400

    added query parsing for new user

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import xss from "xss";
 import validations from "../validate.js";
-import userData from "../data/index.js";
+import { userData } from "../data/index.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
   if (xss(req.session.userID))
     res.render("pages/landing", { title: "Welcome", logedin: true });
-  else res.render("pages/login", { title: "Login" });
+  else {
+    if (req.query.newusercreated)
+      res.render("pages/login", { title: "Login", new_user_created: true });
+    else res.render("pages/login", { title: "Login" });
+  }
 });
 
 router.post("/login", async (req, res) => {
@@ -16,7 +20,7 @@ router.post("/login", async (req, res) => {
     email = validations.checkStevensMail(xss(req.body.login_email));
     password = validations.checkString(xss(req.body.login_password));
 
-    let userExist = await userData.userData.checkUser(email, password);
+    let userExist = await userData.checkUser(email, password);
 
     if (userExist.userAuthenticated && userExist.userAuthenticated) {
       req.session.userID = userExist.userAuthenticated;
@@ -32,9 +36,9 @@ router.post("/login", async (req, res) => {
 router.get("/logout", async (req, res) => {
   if (xss(req.session.userID)) {
     req.session.destroy();
-    res.render("pages/logout", { title: "Loged out" });
-  } else {
+    // res.render("pages/logout", { title: "Loged out" });
     res.redirect("/");
+  } else {
   }
 });
 
