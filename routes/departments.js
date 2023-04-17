6adc114078c17ee8fda3c149c0d53825ec730b77
Priyank Thakur <priyankthakur3@live.com>
2023-04-17 15:33:01 -0400

    tweaked routes

--- routes/departments.js
+++ routes/departments.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
 import { departmentData } from "../data/index.js";
+import xss from "xss";
 
 const router = Router();
 
 router.route("/").get(async (req, res) => {
-  const allDepartmments = await departmentData.ge;
-});
-
-router.route("/create").post(async (req, res) => {
-  if (xss(req.session.userID && req.session.userRole.includes("Admin"))) {
+  if (xss(req.session.userID)) {
+    res.render("pages/departments", { title: "department", logedin: true });
   } else {
+    res.redirect("/");
   }
 });
+
+router.route("/getAll").get(async (req, res) => {
+  let allDepartments;
+  try {
+    allDepartments = await departmentData.getDepartmentAll();
+  } catch (error) {
+    return res.status(500).json({ error: "error" });
+  }
+  return res.json(allDepartments);
+});
+
+export default router;
