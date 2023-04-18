63dc07619e15db968106fa1b06865547462fd34e
Priyank Thakur <priyankthakur3@live.com>
2023-04-17 23:21:18 -0400

    added new validation

--- data/departments.js
+++ data/departments.js
@@ -3,19 +3,24 @@ import validation from "../validate.js";
 import roomDataFunction from "./rooms.js";
 import { departments } from "../config/mongoCollections.js";
 const exportedMethods = {
-  async create(depart_name, room_id, desc, type) {
+  async create(depart_name, room_id, desc, type, operating_hours) {
     depart_name = validation.checkString(depart_name, "Department Name");
     room_id = validation.checkId(room_id, "Room ID");
     desc = validation.checkString(desc, "Department Description");
     type = validation.checkDepartmentType(type);
-
+    operating_hours = validation.checkStringArray(
+      operating_hours,
+      "Operating Hours",
+      2
+    );
+    validation.checkOperatingTimes(operating_hours[0], operating_hours[1]);
     //check if room_id exists
-    try {
-      let dbRoom = await roomDataFunction.getById(room_id);
-      if (!dbRoom) throw `Room doesnot exists for room_id:${room_id}`;
-    } catch (e) {
-      throw e;
-    }
+    // try {
+    //   let dbRoom = await roomDataFunction.getById(room_id);
+    //   if (!dbRoom) throw `Room doesnot exists for room_id:${room_id}`;
+    // } catch (e) {
+    //   throw e;
+    // }
 
     const date = new Date();
     date.setTime(date.getTime() + -240 * 60 * 1000);
@@ -64,6 +69,33 @@ const exportedMethods = {
       department._id = department._id.toString();
     });
     return departmentList;
+  },
+
+  async getDepartmentPaginate(skipRecords, limitRecords) {
+    if (
+      typeof skipRecords !== "number" ||
+      isNaN(skipRecords) ||
+      skipRecords < 0
+    )
+      throw new Error(`Please Pass Proper parameters`);
+    if (
+      !limitRecords ||
+      typeof limitRecords !== "number" ||
+      isNaN(limitRecords) ||
+      limitRecords <= 0
+    )
+      throw new Error(`Please Pass Proper parameters`);
+    const departmentCollection = await departments();
+    const departmentCount = await departmentCollection.count();
+    const departmentList = await departmentCollection
+      .find({})
+      .skip(skipRecords)
+      .limit(limitRecords)
+      .toArray();
+    departmentList.map((department) => {
+      department._id = department._id.toString();
+    });
+    return { totalRecords: departmentCount, departments: departmentList };
   },
 };
 
